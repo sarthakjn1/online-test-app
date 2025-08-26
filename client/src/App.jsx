4e2ff405a4e09b0a4273ecb984b0301a4c097ba7
Jay Kosamkar <jaykosamkar15@gmail.com>
2025-08-26 23:21:32 +0530

    instruction code added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import { Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
 import Register from './components/Register';
 import Login from './components/Login';
+
 import Instructions from './components/Instructions';
 
 
@@ -18,7 +19,9 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/instructions" element={<Instructions />} />
+
+        <Route path  = "/login/instructions" element={<Instructions />}/>
+
         </Routes>
       </BrowserRouter>
     </>
